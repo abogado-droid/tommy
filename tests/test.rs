c1091996d852144c7786a230ab78ddfc0e0a04a9
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-01-11 06:46:24 +0100

    refactor test and readme example

--- tests/test.rs
+++ tests/test.rs
@@ -1,39 +1,32 @@
 use tommy::*;
 
 #[test]
-fn read_the_test_file() {
-    #[derive(Debug)]
-    #[allow(unused)]
-    struct Cursor {
-        blink: bool,
-        blink_duration: i32,
+fn example() {
+    macro_rules! config_table {
+        ($nme:ident { $($fld:ident : $typ:ty),* $(,)? }) => {
+            #[derive(Debug)]
+            #[allow(unused)]
+            struct $nme {
+            $($fld: $typ),*
+            }
+            from_table_struct!($nme {
+            $($fld: $typ),*
+            });
+        };
     }
-    from_table_struct!(Cursor {
+
+    config_table!(Cursor {
         blink: bool,
         blink_duration: i32,
     });
 
-    #[derive(Debug)]
-    #[allow(unused)]
-    struct Window {
-        title: String,
-        width: f64,
-        height: f64,
-    }
-    from_table_struct!(Window {
+    config_table!(Window {
         title: String,
         width: f64,
         height: f64,
     });
 
-    #[derive(Debug)]
-    #[allow(unused)]
-    struct Icons {
-        entry: char,
-        exit: char,
-        controls: char,
-    }
-    from_table_struct!(Icons {
+    config_table!(Icons {
         entry: char,
         exit: char,
         controls: char,
@@ -42,16 +35,22 @@ fn read_the_test_file() {
     let parsed_user = ParseConfig::from_file("test.toml".to_string()).unwrap();
     let parsed_fabk = ParseConfig::from_file("fallback.toml".to_string()).unwrap();
 
-    let cursor_conf: Cursor = parsed_user
-        .table("cursor")
-        .or_else(|| parsed_fabk.table("cursor"))
-        .unwrap();
+    macro_rules! load_conf {
+        ($var:ident : $ty:ty) => {
+            let $var: $ty = parsed_user
+            .table(stringify!($ty).to_lowercase().as_str())
+            .or_else(|| {
+            parsed_fabk.table(stringify!($ty).to_lowercase().as_str())
+            })
+            .unwrap();
+        };
+    }
 
-    // let window_conf: Window = parsed.table("window").unwrap();
-    // let icons_conf: Icons = parsed.table("icons").unwrap();
+    load_conf!(cursor_conf: Cursor);
+    load_conf!(window_conf: Window);
+    load_conf!(icons_conf: Icons);
 
-    // assert_eq!(cursor_conf.blink, true);
     println!("{:#?}", cursor_conf);
-    // println!("{:#?}", window_conf);
-    // println!("{:#?}", icons_conf);
+    println!("{:#?}", window_conf);
+    println!("{:#?}", icons_conf);
 }
